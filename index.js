284f21bd6d228652259ac73f058d442a5d5756f2
suphaluek147-lgtm <suphaluek147@gmail.com>
2025-11-21 20:49:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
+app.get("/", (req, res) => {
+    res.send("Server is running OK");
+});
