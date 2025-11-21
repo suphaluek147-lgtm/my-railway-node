6c3ac0678123a76ebe1e8c6d2c5848003126ea4c
suphaluek147-lgtm <suphaluek147@gmail.com>
2025-11-21 17:51:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Server Running OK!");
+const TOKEN = "8496137521:AAEwyr8ZG42STcYkUKqzW70MRgJmVHUsnxg"; 
+const TELEGRAM_URL = `https://api.telegram.org/bot${TOKEN}`;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Bot Running OK!");
+});
+
+// เมื่อ Telegram ส่งข้อความเข้ามา
+app.post(`/webhook`, async (req, res) => {
+  const message = req.body.message;
+
+  if (message) {
+    const chatId = message.chat.id;
+    const text = message.text;
+
+    await axios.post(`${TELEGRAM_URL}/sendMessage`, {
+      chat_id: chatId,
+      text: "บอทตอบกลับ: " + text,
+    });
+  }
+
+  return res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+app.listen(PORT, () => {
+  console.log(`Bot running on port ${PORT}`);
 });
